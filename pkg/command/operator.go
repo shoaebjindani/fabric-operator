327c562033a8f28da08936d9f9f10b9baccc7976
Shoaeb Jindani <jindani.shoaeb@ibm.com>
2024-04-09 12:34:20 +0530

    added fix for fixpacks

--- pkg/command/operator.go
+++ pkg/command/operator.go
@@ -24,14 +24,17 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strings"
 	"time"
 
 	"gopkg.in/yaml.v2"
 	k8sruntime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	routev1 "github.com/openshift/api/route/v1"
 	"github.com/operator-framework/operator-lib/leader"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
@@ -54,6 +57,8 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
@@ -342,8 +347,32 @@ func GetOperatorNamespace() (string, error) {
 }
 func CheckForFixPacks(config *rest.Config, operatornamespace string) {
 	clientset, err := kubernetes.NewForConfig(config)
+
+	// Create a dynamic client
+	dynamicClient := dynamic.NewForConfigOrDie(config)
+
+	// Define your custom resource type
+	//customResourceName := "ibpconsoles"
+	customResourceNamespace := "ibmsupport"
+	gvr := schema.GroupVersionResource{
+		Group:    "ibp.com",
+		Version:  "v1beta1",
+		Resource: "ibpconsoles",
+	}
+
+	// Retrieve the list of objects in your custom resource
+	list, err := dynamicClient.Resource(gvr).Namespace(customResourceNamespace).List(context.Background(), metav1.ListOptions{})
 	if err != nil {
-		panic(err)
+		panic(err.Error())
+	}
+
+	var consoleObjectName string
+	// Print the names of all objects
+	for _, obj := range list.Items {
+
+		consoleObjectName = obj.GetName()
+		// If you want to do something with the object, you can access it here
+
 	}
 	log.Info(fmt.Sprintf("Latest Console Tag is %s", console.GetImages().ConsoleTag))
 	m, err := util.GetConfigMap(clientset, operatornamespace, "ibm-hlfsupport-console-deployer")
@@ -366,7 +395,42 @@ func CheckForFixPacks(config *rest.Config, operatornamespace string) {
 	//if the latest console tag and the mustgather tag are not same, then we will delete the below two configmaps
 	if console.GetImages().ConsoleTag != mustgathertag {
 		log.Info(fmt.Sprintf("Console Tag is %s and Mustgater tag is %s", console.GetImages().ConsoleTag, mustgathertag))
-		util.DeleteConfigMapIfExists(clientset, operatornamespace, "ibm-hlfsupport-console-console")
-		util.DeleteConfigMapIfExists(clientset, operatornamespace, "ibm-hlfsupport-console-deployer")
+
+		// set the webhook image here as well
+		// Specify deployment namespace and name
+		namespace := "ibm-hlfsupport-infra"
+		deploymentName := "ibm-hlfsupport-webhook"
+
+		// Retrieve the deployment
+		deployment, err := clientset.AppsV1().Deployments(namespace).Get(context.TODO(), deploymentName, v1.GetOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		existingwebhookimage := strings.Split(deployment.Spec.Template.Spec.Containers[0].Image, ":")[0]
+		existingwebhookimage = existingwebhookimage + ":" + console.GetImages().ConsoleTag
+
+		deployment.Spec.Template.Spec.Containers[0].Image = existingwebhookimage
+
+		// Update the deployment
+		_, err = clientset.AppsV1().Deployments(namespace).Update(context.TODO(), deployment, v1.UpdateOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		util.DeleteConfigMapIfExists(clientset, operatornamespace, consoleObjectName+"-console")
+		util.DeleteConfigMapIfExists(clientset, operatornamespace, consoleObjectName+"-deployer")
+
+	} else {
+		log.Info("Looks like the operator was restarted...")
 	}
+
+}
+
+func containsString(slice []metav1.GroupVersionForDiscovery, s string) bool {
+	for _, item := range slice {
+		if item.GroupVersion == s {
+			return true
+		}
+	}
+	return false
 }
